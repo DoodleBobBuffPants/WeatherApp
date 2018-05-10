ff80091651eb3869cea21cfb9a77d07dddf0399c
DoodleBobBuffPants <ajayseanx@gmail.com>
2018-05-10 19:10:09 +0100

    verify start time

--- weatherApp/src/JourneyAlgorithm.java
+++ weatherApp/src/JourneyAlgorithm.java
@@ -1,11 +1,10 @@
-import java.time.*;
+import java.time.LocalTime;
+import java.time.Duration;
 public class JourneyAlgorithm {
 	public static void main(String[] args) {
 	}
 	public static String checkJourney(LocalTime startTime, Duration duration, WeatherEnum preferredWeather, WeeklyWeatherObj weeklyWeather) {
-		
 		int day = 0;
-		
 		if (validJourney(startTime, duration, preferredWeather, day, weeklyWeather)) return "You have a valid journey";
 		
 		
@@ -13,32 +12,27 @@ public class JourneyAlgorithm {
 		return "";
 	}
 	private static boolean validJourney(LocalTime startTime, Duration duration, WeatherEnum preferredWeather, int day, WeeklyWeatherObj weeklyWeather) {
-		
 		DailyWeatherObj dailyArray[] = weeklyWeather.dailyArray;
-		
 		if (startTime.getHour() % 3 == 0) {
 			startTime = startTime.minusHours(2);
 		} else if (startTime.getHour() % 3 == 2) {
 			startTime = startTime.minusHours(1);
 		}
-		
 		LocalTime endTime = startTime.plus(duration);
-		
 		int startIndex = (startTime.getHour() - 1) / 3;
 		int endIndex = (endTime.getHour() + (endTime.getMinute() > 0 ? 1: 0) - 2) / 3;
-		
 		LocalTime currentTime = startTime;
-		
+		int dayReset = 0;
 		for (int i = startIndex; i <= endIndex; i++) {
-			if (dailyArray[day].weather[i] != preferredWeather) {
+			if (dailyArray[day].weather[i - dayReset] != preferredWeather) {
 				return false;
 			}
 			if (currentTime.getHour() < 24 & currentTime.plusHours(3).getHour() >= 0) {
 				day++;
+				dayReset = i + 1;
 			}
 			currentTime = currentTime.plusHours(3);
 		}
-		
 		return true;
 	}
 }
