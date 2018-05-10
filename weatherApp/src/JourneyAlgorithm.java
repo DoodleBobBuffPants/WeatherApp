980051d9ccf51fb47d97bbbec1fc130d16f1da87
DoodleBobBuffPants <ajayseanx@gmail.com>
2018-05-10 18:31:01 +0100

    verifies initial time

--- weatherApp/src/JourneyAlgorithm.java
+++ weatherApp/src/JourneyAlgorithm.java
@@ -1,13 +1,43 @@
 import java.time.*;
 public class JourneyAlgorithm {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
-	public static boolean checkJourney(ZonedDateTime startTime, Duration duration, WeatherEnum preferredWeather, WeeklyWeatherObj weeklyWeather) {
-		//Takes the duration and start time set in settings and the preferred weather and checks if there is a valid journey
-		//from an object storing the weather data for the day and the next day (in case of overlap)
-		DailyWeatherObj dailyArray[] = weeklyWeather.dailyArray();
+	public static String checkJourney(LocalTime startTime, Duration duration, WeatherEnum preferredWeather, WeeklyWeatherObj weeklyWeather) {
+		
+		int day = 0;
+		
+		if (validJourney(startTime, duration, preferredWeather, day, weeklyWeather)) return "You have a valid journey";
+		
+		
+		
+		return "";
+	}
+	private static boolean validJourney(LocalTime startTime, Duration duration, WeatherEnum preferredWeather, int day, WeeklyWeatherObj weeklyWeather) {
+		
+		DailyWeatherObj dailyArray[] = weeklyWeather.dailyArray;
+		
+		if (startTime.getHour() % 3 == 0) {
+			startTime = startTime.minusHours(2);
+		} else if (startTime.getHour() % 3 == 2) {
+			startTime = startTime.minusHours(1);
+		}
+		
+		LocalTime endTime = startTime.plus(duration);
+		
+		int startIndex = (startTime.getHour() - 1) / 3;
+		int endIndex = (endTime.getHour() - 1) / 3;
+		
+		LocalTime currentTime = startTime;
+		
+		for (int i = startIndex; i <= endIndex; i++) {
+			if (dailyArray[day].weather[i] != preferredWeather) {
+				return false;
+			}
+			if (currentTime.getHour() < 24 & currentTime.plusHours(3).getHour() >= 0) {
+				day++;
+			}
+			currentTime = currentTime.plusHours(3);
+		}
 		
 		return true;
 	}
