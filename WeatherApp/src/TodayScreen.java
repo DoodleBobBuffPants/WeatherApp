56e4450cc0e6e7beca7424e9c0567ac29f517574
DoodleBobBuffPants <ajayseanx@gmail.com>
2018-05-18 21:21:37 +0100

    updated

--- WeatherApp/src/TodayScreen.java
+++ WeatherApp/src/TodayScreen.java
@@ -1,8 +1,8 @@
 //necessary imports
 import javax.swing.*;
-import javax.swing.border.MatteBorder;
 
 import java.awt.*;
+import java.util.Collections;
 import java.util.List;
 
 public class TodayScreen extends JPanel {
@@ -47,12 +47,14 @@ public class TodayScreen extends JPanel {
         bottom.setLayout(new GridLayout(1, 1));
 
         ImageIcon back = new ImageIcon("resources/cc3backbygoogle.png");
-        JButton backbutton = new JButton(back);
+        Image bTemp = back.getImage();
+        bTemp = bTemp.getScaledInstance(back.getIconWidth() * 2, back.getIconHeight() * 2, Image.SCALE_SMOOTH);
+        JButton backbutton = new JButton(new ImageIcon(bTemp));
         
-        backbutton.setBorder(new MatteBorder(0, 2, 2, 0, new Color(198, 240, 254)));
-        backbutton.setBorderPainted(true);
+        backbutton.setBorderPainted(false);
         backbutton.setOpaque(false);
         backbutton.setContentAreaFilled(false);
+        backbutton.setHorizontalAlignment(JButton.LEFT);
         
         backbutton.addActionListener(actionEvent -> backToMain());
 
@@ -67,6 +69,8 @@ public class TodayScreen extends JPanel {
         center.setOpaque(false);
         List<weatherForAThreeHourlyPeriod> periodData = todayWeather.getList();	//list of all information needed
         
+        Collections.sort(periodData, (d1, d2) -> d1.getTime().getHour() - d2.getTime().getHour());	//sorts the data by time
+        
         center.setLayout(new GridLayout(periodData.size(), 2));	//structures the data
         
         //add times to panel, ignoring duplicate
@@ -74,8 +78,9 @@ public class TodayScreen extends JPanel {
         
         for (int i = 0; i < index; i++){
         	
-        	String data = periodData.get(i).getTime().toString() + "    " + periodData.get(i).getTemp();
+        	String data = periodData.get(i).getTime().toString() + "    " + periodData.get(i).getTemp() + " °C";
             JLabel time = new JLabel(data);
+            time.setFont(new Font("charcoal", Font.BOLD | Font.ITALIC, 23));
             time.setHorizontalAlignment(JLabel.CENTER);
             time.setVerticalAlignment(JLabel.CENTER);
             center.add(time);
@@ -90,10 +95,12 @@ public class TodayScreen extends JPanel {
         //creates top panel
         JPanel top = new JPanel();
         top.setOpaque(false);
-        top.setLayout(new BorderLayout());
+        top.setLayout(new GridLayout(2, 1));
 
         //adds day label
         JLabel dayLabel = new JLabel(dayofweek);
+        dayLabel.setFont(new Font("charcoal", Font.BOLD | Font.ITALIC, 23));
+        dayLabel.setSize(dayLabel.getWidth(), dayLabel.getHeight() * 2);
         dayLabel.setHorizontalAlignment(JLabel.CENTER);
         dayLabel.setVerticalAlignment(JLabel.CENTER);
         top.add(dayLabel, BorderLayout.NORTH);
@@ -107,16 +114,23 @@ public class TodayScreen extends JPanel {
         JPanel imageIcon = new JPanel();
         imageIcon.setOpaque(false);
         ImageIcon todayIcon = new ImageIcon(todayWeather.getList().get(0).getIconPath().toString());
-        imageIcon.add(new JLabel(todayIcon));
-        imageIcon.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        Image tiTemp = todayIcon.getImage();
+        tiTemp = tiTemp.getScaledInstance(todayIcon.getIconWidth() * 3, todayIcon.getIconHeight() * 3, Image.SCALE_SMOOTH);
+        imageIcon.add(new JLabel(new ImageIcon(tiTemp)), BorderLayout.NORTH);
+        imageIcon.setAlignmentX(CENTER_ALIGNMENT);
+        imageIcon.setAlignmentY(TOP_ALIGNMENT);
         mainData.add(imageIcon);
 
         //Adds Weather data
         JPanel weatherData = new JPanel();
         weatherData.setOpaque(false);
         weatherData.setLayout(new BoxLayout(weatherData, 1));
-        weatherData.add(new JLabel("High: " + high));
-        weatherData.add(new JLabel("Low: " + low));
+        JLabel hLabel = new JLabel("High: " + high + " °C");
+        JLabel lLabel = new JLabel("Low: " + low + " °C");
+        hLabel.setFont(new Font("charcoal", Font.BOLD | Font.ITALIC, 23));
+        lLabel.setFont(new Font("charcoal", Font.BOLD | Font.ITALIC, 23));
+        weatherData.add(hLabel);
+        weatherData.add(lLabel);
         weatherData.setAlignmentX(LEFT_ALIGNMENT);
         mainData.add(weatherData);
 
