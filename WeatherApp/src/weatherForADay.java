f043af1a8913c2973d84d99adc5b5e151e4c1995
FA1211 <fa352@cam.ac.uk>
2018-05-22 14:10:45 +0100

    added comments

--- WeatherApp/src/weatherForADay.java
+++ WeatherApp/src/weatherForADay.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.Serializable;
 
+/**
+ * This class holds all the information for a given day
+ * It  is used as a convenient wrapper to allow us to easily split
+ * the day of the week as well as tri-hourly info
+ */
 public class weatherForADay implements Serializable {
 	
     private List<weatherForAThreeHourlyPeriod> list = new ArrayList<>();	//list of weather information for each 3-hourly period
